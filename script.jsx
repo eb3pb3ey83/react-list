33ec9038dfa3b8d59ba497ed71d5881cc6de49aa
eb3pb3ey83 <sa84202947@yahoo.com.tw>
2018-01-14 19:49:29 +0800

    Update script.jsx

--- script.jsx
+++ script.jsx
@@ -1,111 +1,37 @@
-var TodoInput = React.createClass({
-  getInitialState: function() {
-    return {
-      text: ''
-    };
-  },
-  onChange:function(e){
-    this.setState({
-      text: e.target.value
-    });
-  },
-  onChange2:function(e){
-    this.setState({
-      text2: parseInt(e.target.value)
-    });
-  },  
-  onSubmit:function(e){
-    e.preventDefault();
-    this.props.addItem(this.state.text,this.state.text2);
-    this.setState({text:''});
-    this.setState({text2:''});
-  },
-  render: function() {
-    return (
-      <form className="todo-input" onSubmit={this.onSubmit}>
-
-        請輸入寵物名:<input
-          type="text"
-          onChange={this.onChange}
-          value={this.state.text}
-          />
-        請輸入cp值:<input
-          type="text"
-          onChange={this.onChange2}
-          value={this.state.text2}
-          />          
-          <button type="submit" >Submit</button>
-      </form>
-    );
+class ImagePreview extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      img: '',
+      file: null,
+    }
+    this.onFileSelected = this.onFileSelected.bind(this);
+    this.onImageLoad = this.onImageLoad.bind(this);
   }
-});
 
-var TodoItem = React.createClass({
-  render: function() {
-    var cn = "todo-item";
-    return (
-      <li>
-        {this.props.children}
-      </li>
-    );
+  onFileSelected (e) {
+    const file = e.target.files.item(0);
+
+    const reader = new FileReader();
+    reader.addEventListener('load', this.onImageLoad);
+    reader.readAsDataURL(file);
+
+    this.setState({ file });
   }
-});
 
-var TodoList = React.createClass({
-  render: function() {
-    return (
-      <ul className="todo-list">
-        {
-          this.props.data.map(function(item){
-            return (
-              <TodoItem key={item.id}>
-                <span>寵物名:{item.text}</span>&nbsp;&nbsp;&nbsp;<span>cp值:{item.text2}</span>
-              </TodoItem>
-            );
-          })
-        }
-      </ul>
-    );
+  onImageLoad (e) {
+    this.setState({ img: e.target.result });
   }
-});
 
-var Todo = React.createClass({
-  getInitialState: function() {
-    return {
-      data: this.props.initialData
-    };
-  },
-  addItem: function(text,text2){
-    var data = this.state.data.concat({
-      id: this.state.data.length+1,
-      text: text,
-      text2: text2
-    });
-    
-    data.sort(this.sortNumber);
-    this.setState({data:data});
-  },
-  sortNumber:function(a,b){
-      if (a['text2'] > b['text2']) {
-            return 1;
-            //a會排在b後面
-      }
-  },
-  render: function() {
+  render() {
     return (
-      <div className="todo">
-        <h1>pokemon go寵物表</h1>
-        <TodoInput addItem={this.addItem}/>
-        <TodoList data={this.state.data}/>
+      <div>
+        <input accept="image/*" type="file" onChange={this.onFileSelected} />
+        <br />
+        <img src={this.state.img} width="300" alt="preview" />
       </div>
     );
   }
-});
+}
 
-var data = [
-  {id:1,text:'慢龍',text2:parseInt("3000")}
-];
-ReactDOM.render(
-  <Todo initialData={data}/>,
-  document.getElementById('content')
-);
+ReactDOM.render(<ImagePreview />, document.querySelector("#content"));
